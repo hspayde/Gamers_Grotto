5125e82bca237ee67232d0ecccc12efa0abd5cae
hspayde <spaydeht@hendrix.edu>
2024-10-11 12:55:31 -0500

    Added title text widget

--- gamers_grotto/lib/widgets.dart
+++ gamers_grotto/lib/widgets.dart
@@ -4,15 +4,14 @@ import 'package:google_fonts/google_fonts.dart';
 class TitleText extends StatelessWidget {
   const TitleText({super.key});
   final String titleText = "Gamer's Grotto";
+  
 
   @override
   Widget build(BuildContext context) => Padding(
     padding: const EdgeInsets.all(8.0),
     child: Text(
       titleText,
-      style: const TextStyle(
-
-      )
+      style: GoogleFonts.sixtyfour(),
     )
   )
 }
