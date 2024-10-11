bde2705a61c76b14dd7a4d8ca363aca218049560
hspayde <spaydeht@hendrix.edu>
2024-10-11 12:45:33 -0500

    Added widgets file and updated pubspec

---
+++ gamers_grotto/lib/widgets.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TitleText extends StatelessWidget {
+  const TitleText({super.key});
+  final String titleText = "Gamer's Grotto";
+
+  @override
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Text(
+      titleText,
+      style: const TextStyle(
+
+      )
+    )
+  )
+}
