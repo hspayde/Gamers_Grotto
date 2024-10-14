ef3366eacb421524bc15e7437127c0a025313e6e
hspayde <spaydeht@hendrix.edu>
2024-10-14 12:56:05 -0500

    Added PlayerAvatar Widget and homescreen image

--- gamers_grotto/lib/screens/home_screen.dart
+++ gamers_grotto/lib/screens/home_screen.dart
@@ -7,15 +7,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const TitleText(),
-      ),
-      body: const Column(
+      body: Stack(
+        key: const Key('hStack'),
         children: <Widget>[
+          Text("Hello", style: TextStyle(color: Colors.white)),
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("lib/assets/pexels-feiwang-15837757.jpg"), //
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
           
         ]
       )
+
     );
   }
 }
