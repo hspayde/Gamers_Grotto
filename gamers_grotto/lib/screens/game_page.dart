9528c6e827bcfdd4e81afbd95ba39b91bbed5eac
hspayde <spaydeht@hendrix.edu>
2024-10-15 17:29:24 -0500

    Finished the Chat Widgets

--- gamers_grotto/lib/screens/game_page.dart
+++ gamers_grotto/lib/screens/game_page.dart
@@ -10,7 +10,7 @@ class GamePage extends StatefulWidget{
 }
 
 class GamePageState extends State<GamePage> {
-
+  TextEditingController chatController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +22,38 @@ class GamePageState extends State<GamePage> {
         title: const TitleText(),
         actions: [
           IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return ChatLogDialog(chatLog: ['hello','please work']);
+                }
+              );
+            },
+            icon: Icon(Icons.chat, color: Theme.of(context).colorScheme.secondary),
+          ),
+          IconButton(
             onPressed: () { Navigator.pop(context);},
             icon: Icon(Icons.home_filled, color: Theme.of(context).colorScheme.secondary),
             ),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).colorScheme.primary,
+        child: Padding(padding: EdgeInsets.all(5),child:Row(
+          children: <Widget>[
+            Flexible(child:
+              TextField(
+                controller: chatController,
+                decoration: const InputDecoration(hintText: 'Type Message Here',),
+              ),
+            ),
+            Flexible(child:
+              ElevatedButton(child: Text('Send Message!'), onPressed: null,),
+            ),
+          ],
+        ),
+      ),
       ),
     );
   }
